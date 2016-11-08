b0024041847ab7c328ed37e487eacaf69d6447d6
Michelle Lawrence <michelle.lawrence@utexas.edu>
2016-11-07 21:40:52 -0600

    Initial commit - all content, add photo functionality

---
+++ js/main.js
@@ -0,0 +1,63 @@
+/**********
+  Main timeline template from 
+  https://codyhouse.co/gem/vertical-timeline/
+***********/
+jQuery(document).ready(function($){
+	var timelineBlocks = $('.cd-timeline-block'),
+		offset = 0.8;
+
+	//hide timeline blocks which are outside the viewport
+	hideBlocks(timelineBlocks, offset);
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame) 
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+	});
+
+	function hideBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		});
+	}
+
+	function showBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+		});
+	}
+
+/**** Carousel logic from Mikhail Niedre via CodepPen ****/
+	/* activate the carousel */
+   $("#modal-carousel").carousel({interval:false});
+
+   /* change modal title when slide changes */
+   $("#modal-carousel").on("slid.bs.carousel", function () {
+        $(".modal-title")
+        .html($(this)
+        .find(".active img")
+        .attr("title"));
+   });
+
+   /* when clicking a thumbnail */
+   $(".thumbnail").click(function(){
+    var content = $(".carousel-inner");
+    var title = $(".modal-title");
+  
+    content.empty();  
+    title.empty();
+  
+  	var id = this.id;  
+     var repo = $("#img-repo .item");
+     var repoCopy = repo.filter("#" + id).clone();
+     var active = repoCopy.first();
+  
+    active.addClass("active");
+    title.html(active.find("img").attr("title"));
+  	content.append(repoCopy);
+
+    // show the modal
+  	$("#modal-gallery").modal("show");
+  });
+});
